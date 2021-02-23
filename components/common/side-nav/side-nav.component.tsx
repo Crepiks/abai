a08146cd3d009dd52ec0e39e69e741219287bb80
Crepiks <sayazhan.onlassyn@mail.ru>
2021-02-23 19:19:11 +0600

    feat: implement opening and closing side nav

--- components/common/side-nav/side-nav.component.tsx
+++ components/common/side-nav/side-nav.component.tsx
@@ -1,10 +1,17 @@
+import { Fragment } from "react";
 import Link from "next/link";
+import classNames from "classnames";
 import styles from "./side-nav.module.css";
 
-export default function SideNav({ className, open = false }) {
-  if (open) {
-    return (
-      <nav className={className + " " + styles.openContainer}>
+export default function SideNav({ className, open = false, onOpen, onLeave }) {
+  return (
+    <Fragment>
+      <nav
+        onMouseLeave={onLeave}
+        className={classNames(styles.sideNav, {
+          [styles.sideNavOpen]: open,
+        })}
+      >
         <span className={styles.link}>
           <Link href="/">Главная</Link>
         </span>
@@ -24,16 +31,16 @@ export default function SideNav({ className, open = false }) {
           <Link href="/">Письма</Link>
         </span>
       </nav>
-    );
-  } else {
-    return (
-      <nav className={className + " " + styles.closedContainer}>
+      <nav
+        onMouseOver={onOpen}
+        className={classNames(className, styles.openButton)}
+      >
         <img
           src="/icons/chevron-right.svg"
           alt="Chevron Icon"
           className={styles.chevron}
         />
       </nav>
-    );
-  }
+    </Fragment>
+  );
 }
--- layouts/main/main.layout.tsx
+++ layouts/main/main.layout.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import SideNav from "../../components/common/side-nav/side-nav.component";
 import styles from "./main.module.css";
 
 export default function MainLayout({ children }) {
+  const [sideNavOpen, setSideNavOpen] = useState(false);
+
   return (
     <div className={styles.container}>
-      <SideNav open={true} className={styles.sideNav} />
+      <SideNav
+        open={sideNavOpen}
+        onOpen={() => setSideNavOpen(true)}
+        onLeave={() => setSideNavOpen(false)}
+        className={styles.sideNav}
+      />
       <main className={styles.main}>{children}</main>
     </div>
   );
